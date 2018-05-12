2d4693c2593f25157ac0066df61b1287ddb41ef4
andydevs <akanshul97@gmail.com>
2018-05-12 00:41:43 -0400

    Implemented grunt build system

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+/**
+ * Note-HEMN
+ *
+ * Note app using the HEMN (Handlebars, Express, Mongo, Node) stack
+ *
+ * Author:  Anshul Kharbanda
+ * Created: 5 - 12 - 2018
+ */
+
+/**
+ * Configures grunt
+ *
+ * @param {object} grunt grunt instance
+ */
+module.exports = function(grunt) {
+    // Initialize grunt
+    grunt.initConfig({
+        babel: {
+            dist: {
+                files: {
+                    'build/index.js': 'src/index.js'
+                }
+            }
+        }
+    })
+
+    // Load npm tasks
+    grunt.loadNpmTasks('grunt-babel')
+
+    // Register tasks
+    grunt.registerTask('build', ['babel'])
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * Note-HEMN
+ *
+ * Note app using the HEMN (Handlebars, Express, Mongo, Node) stack
+ *
+ * Author:  Anshul Kharbanda
+ * Created: 5 - 12 - 2018
+ */
+
+console.log('Hello World!')
