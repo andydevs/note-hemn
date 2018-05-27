c22de8147e141d4fc14ba311aec464b812e045cc
andydevs <akanshul97@gmail.com>
2018-05-26 21:03:39 -0400

    Exporting authenticate as default

--- app/authenticate.js
+++ app/authenticate.js
@@ -8,13 +8,14 @@
  */
 
 /**
- * Authentication middleware
+ * Authentication middleware. Check if user has logged in
+ * Else redirect to login page
  *
  * @param {express.Request} req request object
  * @param {express.Response} res response object
  * @param {Function} next middleware next call
  */
-export function authenticate(req, res, next) {
+export default function authenticate(req, res, next) {
     if (req.session && req.session.user) next()
     else res.redirect('/user/login')
 }
--- app/routes/users.js
+++ app/routes/users.js
@@ -8,7 +8,7 @@
  */
 import { Router } from 'express'
 import { dbConnect, using } from '../db'
-import { authenticate } from '../authenticate'
+import authenticate from '../authenticate'
 import {
     setSessionAndRedirect,
     signupUser,
