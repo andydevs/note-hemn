12219f286e0955fabfa42d607c0dc08a61d41ea5
andydevs <akanshul97@gmail.com>
2018-08-20 20:45:40 -0400

    Added mongoose local signup and login

--- app/model/user.js
+++ app/model/user.js
@@ -10,6 +10,9 @@ import { genSaltSync, hash, compare } from 'bcryptjs'
 import { BCRYPT_SALT_ROUNDS } from '../consts'
 import mongoose from 'mongoose'
 
+// Debug
+const debug = require('debug')('note-hemn:model:user');
+
 // Generate salt
 const SALT = genSaltSync(BCRYPT_SALT_ROUNDS)
 
@@ -26,39 +29,41 @@ let User = new mongoose.Schema({
 
 // Signup new user using local strategy
 User.statics.localSignup = function(name, email, password, verify, cb) {
-    if (password === verify)
-        hash(password, SALT, function(err, passhash) {
-            if (err) cb(err)
-            else User.create({
-                login: {
-                    local: {
-                        email,
-                        passhash
-                    }
-                },
-                name
-            }, cb)
-        })
-    else
-        cb(new Error('Password and verify do not match!'))
+    if (password === verify) hash(password, SALT, (err, passhash) => {
+        debug('Hashed password')
+        if (err) cb(err)
+        else this.create({
+            login: {
+                local: {
+                    email,
+                    passhash
+                }
+            },
+            name
+        }, cb)
+    })
+    else cb(new Error('Password and verify do not match!'))
 }
 
 // Local login
 User.statics.localLogin = function(email, password, cb) {
-    User.findOne({ email }, function(err, user) {
+    this.findOne({ 'login.local.email': email }, (err, user) => {
         if (err) cb(err)
-        else if (user) user.localValid(password, function(err2, valid) {
-            if (err2) cb(err2)
-            else if (valid) cb(null, user)
-            else cb(new Error('User was not found!'))
-        })
+        else if (user) {
+            debug('Found user...')
+            user.localValid(password, (err2, valid) => {
+                if (err2) cb(err2)
+                else if (valid) cb(null, user)
+                else cb(new Error('User was not found!'))
+            })
+        }
         else cb(new Error('User was not found!'))
     })
 }
 
 // Check if local user is valid with password
 User.methods.localValid = function(password, cb) {
-    compare(password, this.passhash, cb)
+    compare(password, this.login.local.passhash, cb)
 }
 
 // Export new User
--- app/routes/users.js
+++ app/routes/users.js
@@ -9,6 +9,9 @@
 import { Router } from 'express'
 import User from '../model/user.js'
 
+// Debugger
+const debug = require('debug')('note-hemn:users')
+
 /**
  * Users router
  */
@@ -24,6 +27,18 @@ export default function() {
     // Post signup
     users.post('/signup', (req, res) => {
         // Signup new user
+        let { name, email, password, verify } = req.body
+        User.localSignup(name, email, password, verify, (err, user) => {
+            if (err) {
+                // Redirect back to signup
+                // TODO: Add flash messaging here
+                res.redirect('/user/signup')
+            } else {
+                // Set session and redirect
+                req.session.user = user
+                res.redirect('/user/profile')
+            }
+        })
     })
 
     // Login user page
@@ -34,6 +49,18 @@ export default function() {
     // Post user login
     users.post('/login', (req, res) => {
         // Login user
+        let { email, password } = req.body
+        User.localLogin(email, password, (err, user) => {
+            if (err) {
+                // Redirect back to login
+                // TODO: Add flash messaging here
+                res.redirect('/user/login')
+            } else {
+                // Set session and redirect
+                req.session.user = user
+                res.redirect('/user/profile')
+            }
+        })
     })
 
     // User logout page
@@ -43,7 +70,8 @@ export default function() {
 
     // User post logout
     users.post('/logout', (req, res) => {
-        // Logout user
+        req.session.destroy()
+        res.redirect('/user/login')
     })
 
     // User get profile
