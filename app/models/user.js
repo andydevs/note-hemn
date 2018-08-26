25c7311ed96ed08617dc5af782dd19b777155d42
andydevs <akanshul97@gmail.com>
2018-08-26 00:50:07 -0400

    Removed redundant imports in user

--- app/models/user.js
+++ app/models/user.js
@@ -6,16 +6,11 @@
  * Author:  Anshul Kharbanda
  * Created: 5 - 12 - 2018
  */
-import { genSaltSync, hash, compare } from 'bcryptjs'
-import { BCRYPT_SALT_ROUNDS } from '../consts'
 import mongoose from 'mongoose'
 import localAuthPlugin from './plugins/local-auth';
 
 // Debug
 const debug = require('debug')('note-hemn:model:user');
-
-// Generate salt
-const SALT = genSaltSync(BCRYPT_SALT_ROUNDS)
 
 // User schema
 let User = new mongoose.Schema({
